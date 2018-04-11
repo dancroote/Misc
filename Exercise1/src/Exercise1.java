a553258a986a12a99fba196bdb30ff438954fdab
dancroote <35961289+dancroote@users.noreply.github.com>
2018-04-11 12:51:06 +0100

    commit 4
    fixed logic error

--- Exercise1/src/Exercise1.java
+++ Exercise1/src/Exercise1.java
@@ -5,7 +5,7 @@ public class Exercise1 {
 	public static void main(String args[]) {
 		
 		//Create array of x,y coordinates for each Exchange and Create empty Object list.
-		int[][] exchangeLocations = {{6,5},{11,11}};
+		int[][] exchangeLocations = {{6,4},{11,11}};
 		ArrayList<Object> exchangeArray = new ArrayList<>();
 		
 		//Populate Object list with Exchanges.
@@ -17,21 +17,20 @@ public class Exercise1 {
 		String output = ShortestExchangeId(exchangeArray);
 		System.out.println("The closest exchange is: " + output); 
 	}
-	
+
 	//Returns ID of closest exchange from list of Exchanges.
 	public static String ShortestExchangeId(ArrayList<Object> exchangeArray) {
 		
 		String shortestExchangeId = "";
 		int[] locationArray = new int[exchangeArray.size()];
-		int distance = locationArray[0];
-		int minDistance = locationArray[0];
+		int minDistance = ((Exchange) exchangeArray.get(0)).getDistance();
 		
 		//Create array of distances of Exchanges, find smallest distance in array and return ID of that Exchange.
 		for(int i=0;i<exchangeArray.size();i++ ) {
 				shortestExchangeId = (((Exchange) exchangeArray.get(0)).getId());
 				locationArray[i] = ((Exchange) exchangeArray.get(i)).getDistance();
-	            distance = locationArray[i];
-	            if(distance < minDistance) {
+	            //System.out.println(locationArray[i]);
+	            if(locationArray[i] < minDistance) {
 	            	minDistance = locationArray[i];
 	            	shortestExchangeId = (((Exchange) exchangeArray.get(i)).getId());
 	            }
